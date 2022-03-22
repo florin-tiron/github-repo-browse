f4cc10667975250c8a15d17974d921f2b005fc2a
florintiron <tironmihailflorin@gmail.com>
2022-03-22 18:16:49 +0200

    improve debounce mechanism; clean test files

--- app/src/main/java/com/florintiron/githubrepobrowse/SearchReposViewModel.kt
+++ app/src/main/java/com/florintiron/githubrepobrowse/SearchReposViewModel.kt
@@ -6,7 +6,6 @@ import com.florintiron.githubrepobrowse.domain.base.Result
 import com.florintiron.githubrepobrowse.domain.search.GetMostStarredRepoUseCase
 import com.florintiron.githubrepobrowse.domain.shared.model.GithubRepoData
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.debounce
@@ -22,42 +21,45 @@ private const val DEBOUNCE_TIME_MILLIS = 300L
 internal class SearchReposViewModel @Inject constructor(
     private val getMostStarredRepoUseCase: GetMostStarredRepoUseCase
 ) : ViewModel() {
-    private val searchQuery = MutableStateFlow("")
 
-    private var searchJob: Job? = null
+    private val searchQuery = MutableStateFlow("")
     private val _uiState = MutableStateFlow(SearchRepoViewState.Empty)
     val uiState: StateFlow<SearchRepoViewState> = _uiState
+
+    init {
+        initSearchQueryFlow()
+    }
 
     fun onSearchForRepository(query: String) {
         if (query.length < MIN_SEARCH_CHAR) {
             return
         }
         searchQuery.value = query
-        showLoadingState()
-        performSearch()
     }
 
-    private fun performSearch() {
-        if (searchJob?.isActive == true) {
-            searchJob?.cancel()
-        }
-
-        searchJob = viewModelScope.launch {
+    private fun initSearchQueryFlow() {
+        viewModelScope.launch {
             searchQuery.debounce(DEBOUNCE_TIME_MILLIS).onEach {
-                val result = getMostStarredRepoUseCase.invoke(it)
-                _uiState.value = when (result) {
-                    is Result.SuccessData -> SearchRepoViewState(
-                        query = it,
-                        searchResults = result.data,
-                        refreshing = false
-                    )
-                    is Result.FailureData -> SearchRepoViewState(
-                        query = it,
-                        refreshing = false,
-                        errorMessage = result.exception.message
-                    )
-                }
+                searchQuery.value = it
+                showLoadingState()
+                performSearch(query = it)
             }.collect()
+        }
+    }
+
+    private suspend fun performSearch(query: String) {
+        val result = getMostStarredRepoUseCase.invoke(query)
+        _uiState.value = when (result) {
+            is Result.SuccessData -> SearchRepoViewState(
+                query = query,
+                searchResults = result.data,
+                refreshing = false
+            )
+            is Result.FailureData -> SearchRepoViewState(
+                query = query,
+                refreshing = false,
+                errorMessage = result.exception.message
+            )
         }
     }
 
